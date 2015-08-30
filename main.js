2151f8c5d8792efcd7a4b1283a04fad2bd312ce2
Nicholas Glover <jacksaintjack@gmail.com>
2015-08-30 05:42:36 -0500

    Started to work on the stress quiz section. I was able to get the drop down box to work like I want it too. Next will be working on templates

--- main.js
+++ main.js
@@ -9,28 +9,11 @@ $(document).ready(function() {
   };
 
   $('.wrapperOne').on('click', 'a', pageHandler);
+  $('.wrapperTwo').on('click', 'a', pageHandler);
 
-  //Tranistion page Animation
-  $(".wrapperOne").animsition({
-
-    inClass               :   'fade-in',
-    outClass              :   'fade-out',
-    inDuration            :    1500,
-    outDuration           :    800,
-    linkElement           :   '.animsition-link',
-    // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
-    loading               :    true,
-    loadingParentElement  :   'body', //animsition wrapper element
-    loadingClass          :   'animsition-loading',
-    unSupportCss          : [ 'animation-duration',
-                              '-webkit-animation-duration',
-                              '-o-animation-duration'
-                            ],
-
-    overlay               :   false,
-
-    overlayClass          :   'animsition-overlay-slide',
-    overlayParentElement  :   'body'
-  });
-
+//Replaces answer in the dropdown-menu with the selection the user makes
+  $(".dropdown-menu li a").click(function(){
+  var selText = $(this).text();
+  $(this).parents('.questionGroups').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+});
 });
